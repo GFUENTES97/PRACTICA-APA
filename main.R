cfd0619f317009eb4d2f952e5a271c3ae7482853
ferret41 <marti.ferret@est.fib.upc.edu>
2019-01-07 11:24:44 +0100

    deleting NULL values

--- main.R
+++ main.R
@@ -10,10 +10,25 @@
 #########################################################################
 
 # Llegim les dades:
-
 Data <- read.csv("adult_data.csv")
 
-# Eliminem valors nulls:
+# Seleccionem les variables a fer servir (eliminem la resta de columnes):
+
+# Eliminem files amb valors NULLS a alguna variable:
+Data <- Data[!grepl("NULL", Data$age),]
+Data <- Data[!grepl("NULL", Data$workclass),]
+Data <- Data[!grepl("NULL", Data$fnlwgt),]
+Data <- Data[!grepl("NULL", Data$education),]
+Data <- Data[!grepl("NULL", Data$education.num),]
+Data <- Data[!grepl("NULL", Data$marital.status),]
+Data <- Data[!grepl("NULL", Data$occupation),]
+Data <- Data[!grepl("NULL", Data$relationship),]
+Data <- Data[!grepl("NULL", Data$race),]
+Data <- Data[!grepl("NULL", Data$sex),]
+Data <- Data[!grepl("NULL", Data$capital.gain),]
+Data <- Data[!grepl("NULL", Data$capital.loss),]
+Data <- Data[!grepl("NULL", Data$hours.per.week),]
+Data <- Data[!grepl("NULL", Data$native.country),]
 
 #########################################################################
 # 2. Perform a basic statistical description
