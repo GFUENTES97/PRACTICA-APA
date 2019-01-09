5eaea8449c3664d9aed2fed669adbfad256d461a
ferret41 <marti.ferret@est.fib.upc.edu>
2019-01-09 20:33:11 +0100

    asad

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 #########################################
 # authors        = 'Martí Ferret'       #
 #                = 'Raúl García'        #
-# date           = '2019-01-12          #
+# date           = '2019-01-11          #
 #########################################
 
 
@@ -10,41 +10,60 @@
 #########################################################################
 
 # Llegim les dades:
-Data <- read.csv("adult_data.csv")
+DataAdult <- read.csv("adult_data.csv")
 
 # Seleccionem les variables a fer servir (eliminem la resta de columnes):
+# Veient que en les variables: fnlwgt, capital-loss, capital-gain
+# hi ha molts valors a 0 i NULLs, hem decidit
+# que no treballariem amb elles, per tant eliminem les columnes corresponents
+# de la matriu:
+DataAdult$fnlwgt <- NULL
+DataAdult$capital.loss <- NULL
+DataAdult$capital.gain <- NULL
+
 
 # Eliminem files amb valors NULLS a alguna variable:
-Data <- Data[!grepl("NULL", Data$age),]
-Data <- Data[!grepl("NULL", Data$workclass),]
-Data <- Data[!grepl("NULL", Data$fnlwgt),]
-Data <- Data[!grepl("NULL", Data$education),]
-Data <- Data[!grepl("NULL", Data$education.num),]
-Data <- Data[!grepl("NULL", Data$marital.status),]
-Data <- Data[!grepl("NULL", Data$occupation),]
-Data <- Data[!grepl("NULL", Data$relationship),]
-Data <- Data[!grepl("NULL", Data$race),]
-Data <- Data[!grepl("NULL", Data$sex),]
-Data <- Data[!grepl("NULL", Data$capital.gain),]
-Data <- Data[!grepl("NULL", Data$capital.loss),]
-Data <- Data[!grepl("NULL", Data$hours.per.week),]
-Data <- Data[!grepl("NULL", Data$native.country),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$age),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$workclass),]
+#DataAdult <- DataAdult[!grepl("NULL", DataAdult$fnlwgt),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$education),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$education.num),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$marital.status),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$occupation),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$relationship),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$race),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$sex),]
+#DataAdult <- DataAdult[!grepl("NULL", DataAdult$capital.gain),]
+#DataAdult <- DataAdult[!grepl("NULL", DataAdult$capital.loss),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$hours.per.week),]
+DataAdult <- DataAdult[!grepl("NULL", DataAdult$native.country),]
+
+# Apliquem One-Hot Encoding:
+dmy <- dummyVars(" ~ .", data = DataAdult)
+DataTraining <- data.frame(predict(dmy, newdata = DataAdult))
 
 #########################################################################
 # 2. Perform a basic statistical description
 #########################################################################
 
+summary(DataAdult)
+# Falta comentar
+
 #########################################################################
 # 3. Choose the resampling method to fit, select and test your models
 #########################################################################
+
+# En el nostre cas les dades de training i de test ja anaven donades.
 
 #########################################################################
 # 4. Visualize the data; if need be, cluster the data
 #########################################################################
 
+
 #########################################################################
 # 5. Perform a full modelling process, using linear/quadratic techniques
 #########################################################################
+
 
 #########################################################################
 # 6. Perform a full modelling process, using non-linear techniques
